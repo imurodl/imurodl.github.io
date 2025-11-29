9eb0befe15fa1454542b953320cc43c03e28a0ba
imurodl <murodlearns@gmail.com>
2025-11-29 20:35:10 +0900

    chore: clean up next.js, prepare for sveltekit

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Configure static HTML export for GitHub Pages
-  output: "export",
-};
-
-module.exports = nextConfig;
